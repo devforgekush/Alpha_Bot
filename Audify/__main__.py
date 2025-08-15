c9b3025dc200dbce12387ffb8a25c6e2f0106e60
alpha455 <your-email@example.com>
2025-08-16 01:38:03 +0530

    Fix config import by adding project root to sys.path

--- Audify/__main__.py
+++ Audify/__main__.py
@@ -1,4 +1,7 @@
 # ---------------------------------------------------------
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # Alphabot - All rights reserved
 # ---------------------------------------------------------
 # This code is part of the Alphabot project.
